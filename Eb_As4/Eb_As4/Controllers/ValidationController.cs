3cdea6ec012c629802c0de23499459e43a3c0c64
Eyob234 <97631413+Eyob234@users.noreply.github.com>
2022-03-22 00:09:02 -0400

    first commit

---
+++ Eb_As4/Eb_As4/Controllers/ValidationController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using accounts_payable.Helpers;
+using accounts_payable.Models.DBGenerated;
+using Microsoft.AspNetCore.Mvc;
+
+namespace accounts_payable.Controllers
+{
+    public class ValidationController : Controller
+    {
+
+            private APSect4Context context;
+            public ValidationController(APSect4Context APSect4Context)
+            {
+                context = APSect4Context;
+            }
+
+            public JsonResult CheckPhoneNumber(Vendor vendor)
+            {
+                string msg = validationHelper.PhoneNumberExists(context, vendor.VendorPhone);
+                if (string.IsNullOrEmpty(msg))
+                {
+                    TempData["okPhoneNumber"] = true;
+                    return Json(true);
+                }
+                else
+                {
+                    return Json(msg);
+                }
+            }
+        }
+    }
+
---
+++ Eb_As4/Eb_As4/Startup.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using accounts_payable.Models.DBGenerated;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Eb_As4
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMemoryCache();
+            services.AddSession();
+            services.AddControllersWithViews();
+
+            services.AddDbContext<APSect4Context>(options =>                                
+            options.UseSqlServer(Configuration.GetConnectionString("VendorDBContext")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseSession();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=AccountsPayable}/{action=VendorsList}/{AccountpayableId?}");
+            });
+        }
+    }
+}
+
